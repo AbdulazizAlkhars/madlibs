e5cd10608489dd6c5ae2bc1264e72613f1dbb6a9
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-04 16:58:52 +0300

    Mistakes Fixed

--- madlibs.py
+++ madlibs.py
@@ -8,13 +8,13 @@ def main():
 	items = input("Enter a noun (plural):")
 	name = input("Enter a name:")
 	scream = input("Enter any sentence:")
-	scream1 = scream.upper()
+	scream = scream.upper()
 	action =input("Enter a verb:")
 
 	print("The story goes...")
 	print("It was ",time,"o'clock when I heard a knock at the door.")
-	print("I opened the door and there was a box full of ",items, " with a note saying From Mr." ,name)
-	print("Just as I closed the door I heard a scream ",scream1)
+	print("I opened the door and there was a box full of ",items, " with a note saying From Mr." ,name.title())
+	print("Just as I closed the door I heard a scream ",scream)
 	print("I froze in place and all I python3could do was ",action) 
 
 
